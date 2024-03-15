8dc9123b55292e7f031298e38cea49af8ae0452e
rsdecoder <raginisampath9@gmail.com>
2024-03-15 12:32:53 +0000

    Added error handling for a non-existent topic, path, posting an empty comment, non-existent article

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -7,6 +7,7 @@ const ErrorPage = ({error}) => {
     return (
         <div className = 'error-page'>
             <p className ='error-paragraph'>{error}</p>
+            <p className ='error-paragraph'>Not a valid path</p>
         </div>
     );
 };
